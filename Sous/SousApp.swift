0dc2c5e463208f7ddff6310ebe1684828a734943
Kyle Graham <kgra9230@uni.sydney.edu.au>
2024-05-04 06:33:22 +1000

    Initial Commit

---
+++ Sous/SousApp.swift
@@ -0,0 +1,17 @@
+//
+//  SousApp.swift
+//  Sous
+//
+//  Created by Kyle Graham on 4/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct SousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
